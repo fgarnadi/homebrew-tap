c0e1b69cb0ef4922d5d475ae8c07f8a6e0abd832
fgarnadi <fajar.garnadi@gmail.com>
2026-02-02 11:19:31 +0700

    Add zotero@7.0.32

---
+++ Casks/zotero@7.0.32.rb
@@ -0,0 +1,34 @@
+cask "zotero@7.0.32" do
+  version "7.0.32"
+  sha256 "a50046a263f1a96deb1d00894fb8d8cd8bc7dc5afe64ff6bbba25f56acb7110d"
+
+  url "https://download.zotero.org/client/release/#{version}/Zotero-#{version}.dmg"
+  name "Zotero"
+  desc "Collect, organise, cite, and share research sources"
+  homepage "https://www.zotero.org/"
+
+  livecheck do
+    url "https://www.zotero.org/download/client/update/0/0/Darwin/0/release/update.xml?force=1"
+    strategy :xml do |xml|
+      xml.get_elements("//update").map { |item| item.attributes["version"] }
+    end
+  end
+
+  auto_updates true
+  conflicts_with cask: [
+    "zotero",
+    "zotero@beta"
+  ]
+
+  app "Zotero.app"
+
+  zap trash: [
+        "~/Library/Application Scripts/org.zotero.SafariExtensionApp.SafariExtension",
+        "~/Library/Application Support/Zotero",
+        "~/Library/Caches/Zotero",
+        "~/Library/Containers/org.zotero.SafariExtensionApp.SafariExtension",
+        "~/Library/Preferences/org.zotero.zotero.plist",
+        "~/Library/Saved Application State/org.zotero.zotero.savedState",
+      ],
+      rmdir: "~/Zotero"
+end
